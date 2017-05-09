31a09afda284a9457bb22ad994d324185afda394
rafia <rafia.anwar95@gmail.com>
2017-05-09 13:15:48 -0400

    initial commit

--- echoserver.py
+++ echoserver.py
@@ -12,7 +12,7 @@ PAT = 'EAAGFBcgLxk0BAJRwEv2snjQ5oyORqgpOZBBTPXy23LPbsT0vYjsEaAJw3BOHfSln1QEoawx8
 @app.route('/', methods=['GET'])
 def handle_verification():
   print "Handling Verification."
-  if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
+  if request.args.get('hub_verify_token', '') == 'my_voice_is_my_password_verify_me':
     print "Verification successful!"
     return 'Its good'
   else:
