05969f404f859dfc614ad33b2c7978c6fcf77889
rafia <rafia.anwar95@gmail.com>
2017-05-11 16:37:29 -0400

    checking for token

--- echoserver.py
+++ echoserver.py
@@ -45,7 +45,6 @@ response = requests.post(
 def handle_verification():
   print "Handling Verification."
   print request.args
-  print request.args.get('hub.verify_token', '')
   if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
     print "Verification successful!"
     return request.args.get('hub.challenge', '')
