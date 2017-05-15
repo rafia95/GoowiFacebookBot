4db939cd92bfee11f841dcac070cc1a4e985a6cc
rafia <rafia.anwar95@gmail.com>
2017-05-15 12:29:28 -0400

    changing sign

--- app.py
+++ app.py
@@ -80,8 +80,9 @@ def handle_messages():
     signature = request.headers.get("X-Hub-Signature")
     payload = request.get_data()
     print payload
-    key = bytes("3d39f740aa5d969e1a5bbb7b7dde643d",encoding="UTF-8")
-    message = bytes(payload,"UTF-8")
+    hiddenkey = "3d39f740aa5d969e1a5bbb7b7dde643d"
+    key = hiddenkey.encode("UTF-8")
+    message = payload.encode("UTF-8")
     digester = hmac.new(key,message,hashlib.sha1)
     signature1 = digester.digest()
     print "signature is coming"
