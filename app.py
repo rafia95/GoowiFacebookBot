5451c7871bc9a36d236cbedb6351d23aa442a9b3
rafia <rafia.anwar95@gmail.com>
2017-05-15 10:32:45 -0400

    req print

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ response = requests.post(
 @app.route('/', methods=['GET'])
 def handle_verification():
   print "Handling Verification."
-  print request.args
+  print request
   if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
     print "Verification successful!"
     return request.args.get('hub.challenge', '')
